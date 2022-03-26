a6afba44b3b021c4869511200e1a3356253b86a0
Maurice Filiatreault Hebert <filiatreaultmaurice@gmail.com>
2022-03-25 21:08:36 -0400

    update

--- app.js
+++ app.js
@@ -18,3 +18,13 @@ function operate(num1, num2, operator) {
   if (operator === '*') return multiply(num1, num2);
   if (operator === '/') return divide(num1, num2);
 }
+
+const btn = document.querySelectorAll('.calculator-btn');
+btn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log(e.currentTarget.innerText);
+    const operation = (document.querySelector('.calculator-result').textContent += e.currentTarget.innerText);
+    const arr = [...operation];
+    return arr;
+  });
+});
